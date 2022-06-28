2449a629ae3280d6163db8f6ec4426cfd5114a8a
mazhaoyuan <mazhaoyuan@huoban.com>
2022-06-29 00:05:19 +0800

    test

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,7 +1,13 @@
+/*
+ * @Author: mazhaoyuan
+ * @Date: 2022-06-28 23:32:57
+ * @Description: 
+ */
 'use strict';
 
 module.exports = core;
 
 function core() {
     // TODO
+    return true
 }
--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -1,7 +1,13 @@
+/*
+ * @Author: mazhaoyuan
+ * @Date: 2022-06-28 23:33:16
+ * @Description: 
+ */
 'use strict';
 
 module.exports = utils;
 
 function utils() {
     // TODO
+    return true
 }
